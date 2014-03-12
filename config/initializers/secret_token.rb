d8cb2167ed9fec4938b4d852e6678be39657eb1a
tilman <tilman@kr23.de>
2014-03-12 12:18:45 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nvd3Trial::Application.config.secret_key_base = '1dc6d47f181aca6b35836636231c5df869e3d330332a950612a25ca5eef355e87955c0b672eaefed873ef6ebc9bdd42548f199de3f6ec29df07a5f814a852120'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nvd3Trial::Application.config.session_store :cookie_store, key: '_nvd3_trial_session'
